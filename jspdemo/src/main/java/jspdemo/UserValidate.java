b124b7483fd02d674188640a85f560abb756a17a
Akulatejesh <85212218+Akulatejesh@users.noreply.github.com>
2024-01-15 17:49:43 +0530

    Delete jspdemo directory

--- jspdemo/src/main/java/jspdemo/UserValidate.java
+++
@@ -1,7 +0,0 @@
-package jspdemo;
-
-public class UserValidate {
-    public boolean isValidUser(String userName, String password) {
-        return userName.equals("tester") && password.equals("tester");
-    }
-}
